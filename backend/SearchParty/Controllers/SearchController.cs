b83c736176dec2b5936434ae419115e54d32f4d5
Simon Harriyott <simon@harriyott.com>
2011-10-21 23:47:14 +0100

    Added search controller to return dummy JSON

---
+++ backend/SearchParty/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace SearchParty.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+
+        public JsonResult Index(string q)
+        {
+            return Json(new
+                            {
+                                results = new
+                                              {
+                                                  result1 = new
+                                                                {
+                                                                    title = "GiveCampUK",
+                                                                    url = "http://givecamp.org.uk",
+                                                                    tags = "charity"
+                                                                }
+                                              }
+                            },
+                        JsonRequestBehavior.AllowGet);
+        }
+    }
+}
