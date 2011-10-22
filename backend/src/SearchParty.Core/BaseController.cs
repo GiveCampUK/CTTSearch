12c866a3d79ef731a499a7665fd73fc207b7bf03
Ben Aston <ben.aston@justgiving.com>
2011-10-22 17:46:32 +0100

    movement of files into core

---
+++ backend/src/SearchParty.Core/BaseController.cs
@@ -0,0 +1,16 @@
+﻿namespace SearchParty.Core
+{
+    using System.Web.Mvc;
+    using Data;
+    using NHibernate;
+
+    public class BaseController : Controller
+    {
+        private ISession _session;
+
+        protected ISession DataSession
+        {
+            get { return _session ?? (_session = NHibernateHelper.OpenSession()); }
+        }
+    }
+}
---
+++ backend/src/SearchParty.Core/Data/Overrides/ResourceOverride.cs
@@ -0,0 +1,38 @@
+﻿namespace SearchParty.Core.Data.Overrides
+{
+    using FluentNHibernate.Automapping;
+    using FluentNHibernate.Automapping.Alterations;
+    using Models;
+
+    public class ResourceOverride : IAutoMappingOverride<Resource>
+    {
+        public void Override(AutoMapping<Resource> mapping)
+        {
+            mapping
+                .Map(q => q.LongDescription)
+                .CustomType(typeof(string))
+                .Length(10000)
+                .Column("LongDescription");
+            mapping
+                .Map(q => q.ShortDescription)
+                .CustomType(typeof(string))
+                .Length(10000)
+                .Column("ShortDescription");     
+            mapping
+                .Map(q => q.Tags)
+                .CustomType(typeof(string))
+                .Length(10000)
+                .Column("Tags");
+            mapping
+                .Map(q => q.Title)
+                .CustomType(typeof(string))
+                .Length(10000)
+                .Column("Title");
+            mapping
+                .Map(q => q.Uri)
+                .CustomType(typeof(string))
+                .Length(10000)
+                .Column("Uri");
+        }
+    }
+}
