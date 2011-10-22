69c30b1c0facfa47ac73caacf5964015d7bdb3ba
Simon Harriyott <simon@harriyott.com>
2011-10-22 17:22:50 +0100

    Started on updating resource

--- backend/src/SearchParty.Core/Commands/ResourceCommand.cs
+++ backend/src/SearchParty.Core/Commands/ResourceCommand.cs
@@ -5,14 +5,7 @@ using SearchParty.Core.Models;
 
 namespace SearchParty.Core.Commands
 {
-    public class ResourceCreateCommand
-    {
-        public object PerformAction(int? id, ISession dataSession)
-        {
-            return new {status = "failed"};
-        }
-    }
-    public class ResourceCommand
+    public class ResourceCommand : ResourceCommandBase
     {
         public object PerformAction(int? id, ISession dataSession)
         {
@@ -36,20 +29,6 @@ namespace SearchParty.Core.Commands
                 }
                 return resource.Select(GenerateResource).ToList();
             }
-        }
-
-        private static object GenerateResource(Resource resource)
-        {
-            return new
-                       {
-                           id = resource.Id,
-                           uri = resource.Uri,
-                           title = resource.Title,
-                           tags = resource.Tags.Tagify(),
-                           shortDescription = resource.ShortDescription,
-                           longDescription = resource.LongDescription,
-                           resourceType = resource.ResourceType
-                       };
         }
     }
 }
--- backend/src/SearchParty/Controllers/ResourceController.cs
+++ backend/src/SearchParty/Controllers/ResourceController.cs
@@ -1,20 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using SearchParty.Core.Commands;
+using SearchParty.Core.Models;
 
 namespace SearchParty.Api.Controllers
 {
     public class ResourceController : BaseController
     {
         private readonly ResourceCommand _resourceCommand;
+        private readonly ResourceUpdateCommand _resourceUpdateCommand;
 
-        public ResourceController() : this(new ResourceCommand())
+        public ResourceController() : this(new ResourceCommand(), new ResourceUpdateCommand())
         {
             
         }
 
-        private ResourceController(ResourceCommand resourceCommand)
+        private ResourceController(ResourceCommand resourceCommand, ResourceUpdateCommand resourceUpdateCommand)
         {
             _resourceCommand = resourceCommand;
+            _resourceUpdateCommand = resourceUpdateCommand;
         }
 
         public JsonResult Index(int? id)
@@ -23,7 +27,25 @@ namespace SearchParty.Api.Controllers
                             JsonRequestBehavior.AllowGet);
         }
 
+        [HttpGet]
+        public ActionResult Update()
+        {
+            return View(new Resource
+                            {
+                                Id=0,
+                                LongDescription = "Long description",
+                                ShortDescription = "Short description",
+                                Uri = "http://givecamp.org.uk",
+                                ResourceType = "Uri",
+                                Tags="charity,event",
+                                Title = "GiveCamp UK"
+                            });
+        }
 
-
+        [HttpPost]
+        public ActionResult Update(Resource resource)
+        {
+            return Json(_resourceUpdateCommand.PerformAction(resource, DataSession));
+        }
     }
 }
