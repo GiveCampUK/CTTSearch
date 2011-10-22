702db37bc3dc78c1195c04346911dedb293839ea
Ben Aston <ben.aston@justgiving.com>
2011-10-22 15:35:08 +0100

    Moved to src and addition of test runner.

---
+++ backend/src/SearchParty.Core/Commands/ResourceCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace SearchParty.Core.Commands
+{
+    using System.Web.Mvc;
+    using Infrastructure;
+
+    public class ResourceCommand : ActionCommand<JsonResult, object>    // TODO NullDto
+    {
+        public override JsonResult PerformAction(object o)      // TODO NullDto
+        {
+            return new JsonResult { };
+        }
+    }
+}
