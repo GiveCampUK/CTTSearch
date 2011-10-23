312b57d7f0e9f371b096ad97d00096535a91cc15
waikien <ceneakor@yahoo.co.uk>
2011-10-23 02:04:49 +0100

    updated category create command and added resource delete and resource create commands

--- backend/src/SearchParty.Core/Commands/ResourceCreateCommand.cs
+++ backend/src/SearchParty.Core/Commands/ResourceCreateCommand.cs
@@ -7,20 +7,20 @@ using SearchParty.Core.Models;
 
 namespace SearchParty.Core.Commands
 {
-    class CategoryCreateCommand : CategoryCommand
+    class ResourceCreateCommand : ResourceCommandBase
     {
-        public object PerformAction(Category category, ISession dataSession) {
+        public object PerformAction(Resource resource, ISession dataSession) {
             // been told that the Id is 0 if there is no value sent
-            if (category.Id == 0) {
+            if (resource.Id == 0) {
                 try {
-                    dataSession.Save(category);
+                    dataSession.Save(resource);
                 }
                 catch (Exception e) {
                     throw (e);
                 }
             }
 
-            return GenerateCategory(category);
+            return GenerateResource(resource);
         }
     }
 }
