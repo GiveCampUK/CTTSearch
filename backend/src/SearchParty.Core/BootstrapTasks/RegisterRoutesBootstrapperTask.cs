11f3947feeb792765ed231b36a3f3223084de9fc
Simon Harriyott <simon@harriyott.com>
2011-10-23 10:48:27 +0100

    Fixed casing controller route

--- backend/src/SearchParty.Core/BootstrapTasks/RegisterRoutesBootstrapperTask.cs
+++ backend/src/SearchParty.Core/BootstrapTasks/RegisterRoutesBootstrapperTask.cs
@@ -20,6 +20,12 @@
             _routes.IgnoreRoute("favicon.ico");
 
             _routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            _routes.MapRoute("resourceIndex", "resource/index/{id}", new { controller = "Resource", action = "Index" });
+            _routes.MapRoute("resourceUpdate", "resource/update/{id}", new { controller = "Resource", action = "Update" });
+            _routes.MapRoute("categoryIndex", "category/index/{id}", new { controller = "Category", action = "Index" });
+            _routes.MapRoute("categoryUpdate", "category/update/{id}", new {controller = "Category", action = "Update"});
+            _routes.MapRoute("category", "category", new {controller = "Category", action = "Index"});
+            _routes.MapRoute("searchLower", "search", new {controller = "Search", action = "SearchEngine"});
             _routes.MapRoute("search", "Search", new {controller = "Search", action = "SearchEngine"});
             _routes.MapRoute("searchQueries", "Search/Last/{count}", new {controller = "Search", action = "Last", count = 50});
             _routes.MapRoute(
